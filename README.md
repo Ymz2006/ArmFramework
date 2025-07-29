a91db15491c10c3b5c34c3dda8368484b26a4cad
Ymz2006 <jeffreyzhou123@gmail.com>
2025-07-29 10:53:21 -0400

    updated README, fixed PID

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ArmFramework
-Robotic arm control framework adapted to control various robotic arms, such as Elite Robots series and JAKA series. 
+Robotic arm control framework adapted to control various robotic arms, such as Elite Robots series and JAKA series. It was used to quickly write routines for experiments and recording demonstrations. 
+
+
+<img src="resources/arm.jpg" width="400"> <br>
+ Here is a demo on the JAKA 5 series robot showcasing the ArUco marker tracking capability. By scaning the position of the ArUco marker, the pinhole camera model formula can be used to calculate the position of the marker assuming it is a flat table since the camera height is provided through the robot.
+
+<img src="resources/mountedcameragif.gif" width="400">
+<img src="resources/orthocameragif.gif" width="400">
+
+This works espically when the camera is obstructed, making it difficult to detect the marker at all times
+<img src="resources/arucodetection.png" width="800">
+
+Another way to guarantee the xy position of the robot is through PID feedback, which is guaranteed to work with any height. This is sometimes used for fine tuning. 
+
+Here is a demo with the Elite Robot ec12, and using the framework it can work with a custom coordinate system as well as relative motion and a custom velocity profile, features that are not provided in the original library. 
+
+
+
+<img src="resources/eccclip3.gif" width="400">
--- src/pid_controller.py
+++ src/pid_controller.py
@@ -1,33 +1,33 @@
 import time
+
 class PID():
-    def __init__(self, target, kp, ki, kd, deadzone):
+    def __init__(self, target, kp, ki, kd, deadzone=0.0):
         self.target = target
         self.kp = kp
         self.ki = ki
         self.kd = kd
-        self.last_error = 0
-        self.integral = 0
-        self.last_update = time.time()
         self.deadzone = deadzone
-        return
-    
-    
-    def update(self, measurment):
-        error = self.target-measurment
-        self.integral += error
 
+        self.last_error = 0.0
+        self.integral = 0.0
+        self.last_update = time.time()
+    
+    def update(self, measurement):
         curr_time = time.time()
         elapsed_time = curr_time - self.last_update
-
         self.last_update = curr_time
-        
-        porportional = self.kp * error
-        integral = self.ki * self.integral
-        derivative = self.kd * (self.last_error - measurment)/elapsed_time 
 
-   
+        error = self.target - measurement
         
-        return porportional+integral+derivative
-    
-    
+        if abs(error) < self.deadzone:
+            error = 0.0
 
+        self.integral += error * elapsed_time
+        derivative = (error - self.last_error) / elapsed_time if elapsed_time > 0 else 0.0
+        self.last_error = error
+
+        p_term = self.kp * error
+        i_term = self.ki * self.integral
+        d_term = self.kd * derivative
+
+        return p_term + i_term + d_term
